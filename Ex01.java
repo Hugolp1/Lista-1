a7fb0a39fdc1c39fc2354cd967c5c1b007c2cafa
Hugolp1 <hugolourencoprado@gmail.com>
2024-03-18 09:27:52 -0300

    Exercício 1

---
+++ Ex01.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Ex01 {
+    public static void executar() {
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Digite um numero: ");
+        double num = leitor.nextDouble();
+
+        if (num > 10) {
+            System.out.println("Número maior do que 10!");
+        } else {
+            System.out.println("Número menor ou igual a 10!");
+        }
+    }
+}
